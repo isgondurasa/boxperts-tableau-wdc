27c758b62da41567a62487b1cf22d9513789491e
a.sviridov <sviridov.ao@yandex.ru>
2016-12-02 10:37:18 +0300

    deploy

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,11 @@ MAINTAINER sviridov.ao@yandex.ru
 RUN apt-get update && apt-get install -y python-pip python-dev build-essential libssl-dev libffi-dev \
 libxml2-dev libxslt1-dev
 
-COPY . /wdc
+ADD requirements.txt /wdc/
+RUN pip install -r requirements.txt
+
+ADD app.tar.bz2 /wdc
 WORKDIR /wdc
 
-RUN pip install -r requirements.txt
+
 
--- fabfile.py
+++ fabfile.py
@@ -2,8 +2,8 @@ import os
 import logging
 import settings
 
-from fabric.context_managers import cs, lcd
-from fabriq.api import (run,
+from fabric.context_managers import cd, lcd
+from fabric.api import (run,
                         env,
                         task,
                         local,
@@ -11,14 +11,23 @@ from fabriq.api import (run,
 from fabric.decorators import hosts
 
 HOST = "root@185.143.173.48"
-HOOST_PATH = "~/wdc"
+HOST_PATH = "~/wdc"
 
 def deploy(branch="master", arch_name="app.tar.bz2"):
     """
     deploy a wdc application
     """
 
-    local("git archive --format="tar" "{}" | bzip > {}".format(branch, arch_name))
+    local('git archive --format="tar" "{}" | bzip2 > {}'.format(branch, arch_name))
 
     remote_path = "{}:{}/".format(HOST, HOST_PATH)
     local('scp {} {}/'.format(arch_name, remote_path))
+    local('scp {} {}/'.format("docker-compose.yml", remote_path))
+    local('scp {} {}/'.format("Dockerfile", remote_path))
+    local('scp {} {}/'.format("web.env", remote_path))
+    local('scp {} {}/'.format("requirements.txt", remote_path))
+
+    with cd ('wdc'):
+        run("docker-compose down")
+        run("docker-compose build")
+        run("docker-compose up --focre-recreate -d")
